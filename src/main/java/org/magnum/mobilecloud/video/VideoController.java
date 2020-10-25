220ce0edf00a3f834378b8f785300333b9749cbb
Malin Michael <mishamalin.dev@gmail.com>
2020-10-25 23:34:22 +0500

    Add likeVideo method

--- src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -1,11 +1,13 @@
 package org.magnum.mobilecloud.video;
 
 
+import com.google.gson.Gson;
 import org.magnum.mobilecloud.video.repository.Video;
 import org.magnum.mobilecloud.video.repository.VideoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +17,7 @@ import retrofit.http.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.*;
 
 
@@ -40,11 +43,32 @@ public class VideoController {
 
     @RequestMapping(value="/video/{id}", method = RequestMethod.GET)
     public @ResponseBody ResponseEntity<Video> getVideoById(
-            @PathVariable long id) throws IOException {
+            @PathVariable long id) {
         Video v = videoRepo.findOne(id);
         if (v != null)
             return new ResponseEntity<>(v, HttpStatus.OK);
 
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
+
+
+    @RequestMapping(value="/video/{id}/like", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<String>  likeVideo(@PathVariable long id,  Principal p){
+        Video v = videoRepo.findOne(id);
+        String userId = p.getName();
+
+        if (v == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Set<String> likedBy = v.getLikedBy();
+        if (likedBy.contains(userId)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        likedBy.add(userId);
+        v.setLikes(v.getLikes() + 1);
+        videoRepo.save(v);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
